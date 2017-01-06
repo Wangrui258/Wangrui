e961aa15c1b6bd722f66e98cf7ab3bd6e2c6c43c
Wangrui258 <1124979056@qq.com>
2017-01-06 11:15:23 +0800

    去去去

--- app/src/main/java/wang/atguigu/com/wangrui/MainActivity.java
+++ app/src/main/java/wang/atguigu/com/wangrui/MainActivity.java
@@ -1,7 +1,7 @@
 package wang.atguigu.com.wangrui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.print("我的代码");
+
     }
 }
